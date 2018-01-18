8e6bd3be165aead1b93f8e890079c1ed164e3298
zc <340355960@qq.com>
2018-01-18 21:55:33 +0800

    9:03

--- run.sh
+++ run.sh
@@ -1,7 +1,3 @@
 #!/bin/bash  
 
-(nohup ruby main.rb repo0.csv > output0 2>&1 & )&
-(nohup ruby main.rb repo1.csv > output1 2>&1 & )&
-(nohup ruby main.rb repo2.csv > output2 2>&1 & )&
-(nohup ruby main.rb repo3.csv > output3 2>&1 & )&
-(nohup ruby main.rb repo4.csv > output4 2>&1 & )&
+(nohup ruby main.rb test.csv > output 2>&1 & )&
