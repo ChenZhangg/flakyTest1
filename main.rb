0e567c7f7d4122f48816c23329422c07d45ac346
zc <340355960@qq.com>
2018-01-30 21:18:15 +0800

    9:18

--- main.rb
+++ main.rb
@@ -8,7 +8,7 @@ require 'travis/client'
 
 @number_of_repositories=1
 @client = Travis::Client.new
-
+@github_username=ChenZhangg
 def clone_repo(user_dir,repo_url,repo_dir)
   i=0
   while(i<600)
@@ -36,7 +36,7 @@ def create_github_repo(repo_dir,user_name,repo_name,i)
       sleep 60
       next
     else
-      puts "*******************Create repo ChenZhangg/#{user_name}_#{repo_name}_#{i} on Github successfully.**************************"
+      puts "*******************Create repo #{@github_username}/#{user_name}_#{repo_name}_#{i} on Github successfully.**************************"
       break
     end
   end
@@ -45,14 +45,14 @@ end
 def open_travis(repo_dir,user_name,repo_name,i)
   k=0
   while(k<600)
-    %x"cd #{repo_dir} && travis enable -r ChenZhangg/#{user_name}_#{repo_name}_#{i}"
+    %x"cd #{repo_dir} && travis enable -r #{@github_username}/#{user_name}_#{repo_name}_#{i}"
     if $?.to_i!=0
-      puts "*******************The #{k} of 600 try to enable ChenZhangg/#{user_name}_#{repo_name}_#{i}. $?=#{$?.to_i}*******************************"
+      puts "*******************The #{k} of 600 try to enable #{@github_username}/#{user_name}_#{repo_name}_#{i}. $?=#{$?.to_i}*******************************"
       k+=1
       sleep 60
       next
     else
-      puts "*******************Enable travis ChenZhangg/#{user_name}_#{repo_name}_#{i} successfully.**************************"
+      puts "*******************Enable travis #{@github_username}/#{user_name}_#{repo_name}_#{i} successfully.**************************"
       break
     end
   end
@@ -61,15 +61,15 @@ end
 def link_remote(repo_dir,user_name,repo_name,i)
   k=0
   while(k<600)
-    %x"cd #{repo_dir} && git remote add #{user_name}_#{repo_name}_#{i} git@github.com:ChenZhangg/#{user_name}_#{repo_name}_#{i}.git"
+    %x"cd #{repo_dir} && git remote add #{user_name}_#{repo_name}_#{i} git@github.com:#{@github_username}/#{user_name}_#{repo_name}_#{i}.git"
     if $?.to_i==0
-      puts "*******************Remote add ChenZhangg/#{user_name}_#{repo_name}_#{i} successfully.**************************"
+      puts "*******************Remote add #{@github_username}/#{user_name}_#{repo_name}_#{i} successfully.**************************"
       break
     elsif $?.to_i==128
-      puts "*******************Already add ChenZhangg/#{user_name}_#{repo_name}_#{i}.**************************"
+      puts "*******************Already add #{@github_username}/#{user_name}_#{repo_name}_#{i}.**************************"
       break
     else
-      puts "*******************The #{k} of 600 try to remote add ChenZhangg/#{user_name}_#{repo_name}_#{i}. $?=#{$?.to_i}*******************************"
+      puts "*******************The #{k} of 600 try to remote add #{@github_username}/#{user_name}_#{repo_name}_#{i}. $?=#{$?.to_i}*******************************"
       k+=1
       sleep 60
       next
@@ -90,13 +90,13 @@ def get_last_build(user_name,repo_name,i)
   k=0
   begin
     @client.clear_cache!
-    travis_repo= @client.repo("ChenZhangg/#{user_name}_#{repo_name}_#{i}")
+    travis_repo= @client.repo("#{@github_username}/#{user_name}_#{repo_name}_#{i}")
     last=travis_repo.last_build
   rescue
     puts $!
     travis_repo=nil
     last=nil
-    puts "*******************The #{k} of 10 try to get last build of ChenZhangg/#{user_name}_#{repo_name}_#{i}.**************************"
+    puts "*******************The #{k} of 10 try to get last build of #{@github_username}/#{user_name}_#{repo_name}_#{i}.**************************"
     sleep 60
     k+=1
     retry if k<10
@@ -107,9 +107,9 @@ end
 def pre_build_completed(user_name,repo_name,i)
   sleep 300
   last=get_last_build(user_name,repo_name,i)
-  puts "*******************The information of last build of ChenZhangg/#{user_name}_#{repo_name}_#{i} is #{last ? last.number : '0'}********************"
+  puts "*******************The information of last build of #{@github_username}/#{user_name}_#{repo_name}_#{i} is #{last ? last.number : '0'}********************"
   while(last && last.finished? != true)
-    puts "*******************The last build #{last ? last.number : '0'} of ChenZhangg/#{user_name}_#{repo_name}_#{i} is not finished.********************"
+    puts "*******************The last build #{last ? last.number : '0'} of #{@github_username}/#{user_name}_#{repo_name}_#{i} is not finished.********************"
     sleep 300
     last=get_last_build(user_name,repo_name,i)
   end
@@ -120,20 +120,20 @@ def push(repo_dir,user_name,repo_name,l,i)
   while(k<5)
     %x"cd #{repo_dir} && git push #{user_name}_#{repo_name}_#{i} #{l.sha}:refs/heads/master"
     if $?.to_i==0
-      puts "*******************Push #{l.sha}  #{l.date} to Github repo ChenZhangg/#{user_name}_#{repo_name}_#{i}*******************"
+      puts "*******************Push #{l.sha}  #{l.date} to Github repo #{@github_username}/#{user_name}_#{repo_name}_#{i}*******************"
       break
     elsif $?.to_i==256
-      puts "*******************Push Error $?=#{$?} #{l.sha}  #{l.date} to Github repo ChenZhangg/#{user_name}_#{repo_name}_#{i}*******************"
+      puts "*******************Push Error $?=#{$?} #{l.sha}  #{l.date} to Github repo #{@github_username}/#{user_name}_#{repo_name}_#{i}*******************"
       k+=1
       sleep 60
       next
     elsif $?.to_i==32768
-      puts "*******************Push Error $?=#{$?.to_i} #{l.sha}  #{l.date} to Github repo ChenZhangg/#{user_name}_#{repo_name}_#{i}*******************"
+      puts "*******************Push Error $?=#{$?.to_i} #{l.sha}  #{l.date} to Github repo #{@github_username}/#{user_name}_#{repo_name}_#{i}*******************"
       k+=1
       sleep 60
       next
     else
-      puts "*******************Push Error $?=#{$?.to_i} #{l.sha}  #{l.date} to Github repo ChenZhangg/#{user_name}_#{repo_name}_#{i}*******************"
+      puts "*******************Push Error $?=#{$?.to_i} #{l.sha}  #{l.date} to Github repo #{@github_username}/#{user_name}_#{repo_name}_#{i}*******************"
       k+=1
       sleep 60
       next
